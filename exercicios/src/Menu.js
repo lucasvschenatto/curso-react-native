c01b66cc00fcdd4f5c0f588aa5c0ea7fcc923555
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-01-14 19:36:41 -0200

    finished lesson 23

---
+++ exercicios/src/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {createDrawerNavigator} from 'react-navigation';
+import Simple from './components/Simple';
+import EvenOdd from './components/EvenOdd';
+import Invert, { Hi, MegaSena, functionThree } from './components/Multi';
+
+export default createDrawerNavigator({
+    MegaSena: {
+        screen: () => <MegaSena numbers={8} />,
+        navigationOptions: {title: 'Mega Sena'}
+    },
+    Invert: {
+        screen: () => <Invert text='React Native!'/>
+    },
+    EvenOdd: {
+        screen: () => <EvenOdd number={4}/>,
+        navigationOptions: {title: 'Even & Odd'}
+    },
+    Simple:{
+        screen: () => <Simple customText='Screen with simple component'/>
+    },
+    Hi: {
+        screen: Hi
+    }
+}, {drawerWidth: 300});
