b4c4a0fa2cde958071a07f9a2d30edd2c24c0364
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-02-01 01:28:24 -0200

    lesson 44

--- calculator/App.js
+++ calculator/App.js
@@ -15,12 +15,16 @@ export default class App extends Component{
   state = { ...initialState }
 
   addDigit = number => {
-    if(number === '.' && this.state.displayValue.includes('.')){
+    console.debug(typeof this.state.displayValue)
+    
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+    
+    if(number === '.' && !clearDisplay && this.state.displayValue.includes('.')){
       return
     }
 
-    const clearDisplay = this.state.displayValue === '0'
-      || this.state.clearDisplay
+
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + number
     this.setState({displayValue, clearDisplay: false})
@@ -38,7 +42,26 @@ export default class App extends Component{
   }
 
   setOperation = operation => {
+    if(this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    }else{
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try{
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch(exception){
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
   render() {
     return (
