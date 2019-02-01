7661d111864a1e202f348a35ab69ccac4d261a77
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-02-01 17:21:33 -0200

    lesson 48

--- mines/App.js
+++ mines/App.js
@@ -11,6 +11,10 @@ export default class App extends Component {
         <Text style={styles.welcome}>Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
         <Field />
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
       </View>
     );
   }
