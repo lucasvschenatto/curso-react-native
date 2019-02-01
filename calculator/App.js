0397cc82b29ebdec89dcbe487e829de8931cc86c
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-02-01 00:03:10 -0200

    lesson 41

--- calculator/App.js
+++ calculator/App.js
@@ -3,17 +3,38 @@ import {StyleSheet, Text, View} from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0,0],
+  current: 0
+}
+
 export default class App extends Component{
-  state = {
-    displayValue: '0'
-  }
+  state = { ...initialState }
 
   addDigit = number => {
-    this.setState({displayValue: number})
+    if(number === '.' && this.state.displayValue.includes('.')){
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + number
+    this.setState({displayValue, clearDisplay: false})
+
+    if (number !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({values})
+    }
   }
 
   clearMemory = () => {
-    this.setState({displayValue: '0'})
+    this.setState({...initialState})
   }
 
   setOperation = operation => {
